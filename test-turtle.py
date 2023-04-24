4d97c5d1015b8e0e5e68aae3f4a7207ef06d213e
HallZero <filipikikuchi@gmail.com>
2023-04-23 23:20:48 -0300

    Add drawing method for testing purposes

--- test-turtle.py
+++ test-turtle.py
@@ -1,6 +1,7 @@
 import rclpy
 from random import random
 from rclpy.node import Node
+from geometry_msgs.msg import Twist
 from turtlesim.srv import TeleportAbsolute, SetPen, Spawn
 
 class TurtleHub(Node):
@@ -9,6 +10,8 @@ class TurtleHub(Node):
         self.client_tp = self.create_client(TeleportAbsolute, '/turtle1/teleport_absolute')
         self.client_pen = self.create_client(SetPen, '/turtle1/set_pen')
         self.client_spawn = self.create_client(Spawn, '/spawn')
+        self.publisher_draw = self.create_publisher(Twist, 'turtle2/cmd_vel', 10)
+        self.draw()
 
     def togglePen(self):
         req = SetPen.Request()
@@ -29,22 +32,30 @@ class TurtleHub(Node):
 
         return req
     
-    def spawn(self):
+    def spawn(self, x, y, theta):
         req = Spawn.Request()
-        req.x = 2.0
-        req.y = 8.0
-        req.theta = 0.0
+        req.x = x
+        req.y = y
+        req.theta = theta
         req.name = 'turtle2'
 
         return req
+    
+    def draw(self):
+        twist_msg_ = Twist()
+
+        twist_msg_.linear.x = 1.0
+        twist_msg_.angular.z = 0.0
+        self.publisher_draw.publish(twist_msg_)
 
 
 def main(args=None):
     rclpy.init(args=args)
     turtle_hub = TurtleHub()
-    future = turtle_hub.client_spawn.call_async(turtle_hub.spawn())
+    future = turtle_hub.client_spawn.call_async(turtle_hub.spawn(4.0, 9.0, 4.71))
     rclpy.spin_until_future_complete(turtle_hub, future)
-    #rclpy.spin(turtle_hub)
+    #rclpy.spin_once(turtle_hub)
+    rclpy.spin(turtle_hub)
     result = future.result()
     turtle_hub.destroy_node()
     rclpy.shutdown()
